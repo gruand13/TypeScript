7b0e3881e2533eda55e30b3ed0c017b68a267cd8
Andrey Hrusheuski <gruand13@gmail.com>
2023-03-25 19:14:13 +0300

    functions

--- index.ts
+++ index.ts
@@ -1,6 +1,31 @@
+// let userName: string = "Andrey";
 
+// userName = 5;
+// userName.isInteger();
 
+// ``, '', ""
+// 10, 0.5, 0.00001, -50, 4e10
 
-const str  ="str";
+// true,false
+const isBirthdayData: boolean =true;
+const ageData: number=40;
+const userNameData: string = 'John';
 
-console.log(str);
+// function logBrtMsg(isBirthday: any,age:number, userName: string): string {
+//     if (isBirthday) {
+//         return `Congrats ${userName.toUpperCase()}, age: ${age+1}`;
+//     } else {
+//         return "Error";
+//     }
+    
+// }
+
+const logBrtMsg =(isBirthday: any,age:number, userName: string): string => {
+    if (isBirthday) {
+        return `Congrats ${userName.toUpperCase()}, age: ${age+1}`;
+    } else {
+        return "Error";
+    }
+    
+}
+logBrtMsg(isBirthdayData, ageData, userNameData);
