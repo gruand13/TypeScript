77d84b69bbda1c97c1f9b8d6b9e3c253f3bf38d0
Andrey Hrusheuski <gruand13@gmail.com>
2023-03-26 20:10:22 +0300

    null and undefined

--- index.ts
+++ index.ts
@@ -4,6 +4,13 @@ const test2 : any = null;
 const test3 : string = null;
 const test4 : number = null;
 
+const test5 : undefined= undefined;
+const test6 : any= undefined;
+const test7 : string= undefined;
+
+let smth;
+
+
 function getRandomData(){
     if (Math.random()< 0.5){
         return null;
