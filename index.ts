2b13146960d027dff4fc390e14ff714bbf862bd2
Andrey Hrusheuski <gruand13@gmail.com>
2023-03-26 15:03:58 +0300

    convert eur to usd

--- index.ts
+++ index.ts
@@ -1,45 +1,18 @@
-// let userName: string = "Andrey";
+const currRate = "1.05";
 
-// userName = 5;
-// userName.isInteger();
+const fetchCurr = (response:string): number => {
+	const data = JSON.parse(response);
+	return data;
+};
 
-// ``, '', ""
-// 10, 0.5, 0.00001, -50, 4e10
-// true,false
-
-
-// const isBirthdayData: boolean =true;
-// const ageData: number=40;
-// const userNameData: string = 'John';
-
-// function logBrtMsg(isBirthday: any,age:number, userName: string): string {
-//     if (isBirthday) {
-//         return `Congrats ${userName.toUpperCase()}, age: ${age+1}`;
-//     } else {
-//         return "Error";
-//     }
-    
-// }
-let salary: number;
-salary=5000;
-
-
-const userData={"isBirthdayData": true, "ageData": 40, "userNameData": "John"};
-const json="sakflgl";
-const userObj:{
-    isBirthdayData: boolean,
-    ageData:number,
-     userNameData: string 
-} = JSON.parse(userData);
-
-
-
-const logBrtMsg =(isBirthday: any,age:number, userName: string): string => {
-    if (isBirthday) {
-        return `Congrats ${userName.toUpperCase()}, age: ${age+1}`;
-    } else {
-        return "Error";
-    }
-    
+function transferEurToUsd(available: boolean, amount:number, commission:number): void {
+	if (available) {
+		let res = fetchCurr(currRate) * amount * commission;
+		console.log(res);
+		// Или запись в элемент на странице вместо консоли
+	} else {
+		console.log("Сейчас обмен недоступен");
+	}
 }
-logBrtMsg(isBirthdayData, ageData, userNameData);
+
+transferEurToUsd(true, 500, 1.05);
