fdef3b4268ec1c51609880085df537fb6fb996bb
Andrey Hrusheuski <gruand13@gmail.com>
2023-03-29 20:34:29 +0300

    objects and destructurization

--- index.ts
+++ index.ts
@@ -1,45 +1,35 @@
-// let userName: string = "Andrey";
-
-// userName = 5;
-// userName.isInteger();
-
-// ``, '', ""
-// 10, 0.5, 0.00001, -50, 4e10
-// true,false
-
-
 // const isBirthdayData: boolean =true;
 // const ageData: number=40;
 // const userNameData: string = 'John';
 
-// function logBrtMsg(isBirthday: any,age:number, userName: string): string {
-//     if (isBirthday) {
-//         return `Congrats ${userName.toUpperCase()}, age: ${age+1}`;
-//     } else {
-//         return "Error";
-//     }
-    
-// }
-let salary: number;
-salary=5000;
+const userData={
+    isBirthdayData: true,
+    ageData: 40,
+    userNameData: 'John',
+    messages: {
+        error:"Error"
+    }
 
+}
+const createError=(msg: string)=>{
+    throw new Error(msg);
+}
 
-const userData={"isBirthdayData": true, "ageData": 40, "userNameData": "John"};
-const json="sakflgl";
-const userObj:{
+function logBrtMsg({isBirthdayData,
+                    userNameData,
+                     ageData,
+                    messages:{error}}: {
     isBirthdayData: boolean,
+    userNameData: string,
     ageData:number,
-     userNameData: string 
-} = JSON.parse(userData);
-
-
-
-const logBrtMsg =(isBirthday: any,age:number, userName: string): string => {
-    if (isBirthday) {
-        return `Congrats ${userName.toUpperCase()}, age: ${age+1}`;
+    messages: {error: string}
+}): string {
+    if (isBirthdayData===true) {
+	 return `Congrats ${userNameData.toUpperCase()}, age: ${ageData+1}`;
     } else {
-        return "Error";
+        return createError(error);
     }
     
+
 }
-logBrtMsg(isBirthdayData, ageData, userNameData);
+logBrtMsg(userData);
