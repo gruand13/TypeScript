8c7b64f3e2658f8efe050aa67bd1ea0b4d58014c
Andrey Hrusheuski <gruand13@gmail.com>
2023-04-02 12:25:48 +0300

    tuples

--- index.ts
+++ index.ts
@@ -1,50 +1,68 @@
-const electricityUserData = {
-	readings: 95,
-	units: "kWt",
-	mode: "double",
-};
 
-const waterUserData = {
-	readings: 3,
-	units: "m3",
-};
+const userData={
+    isBirthdayData: true,
+    ageData: 40,
+    userNameData: 'John',
+    messages: {
+        error:"Error"
+    }
 
-const elRate: number = 0.45;
-const wRate: number = 2;
+}
 
-const monthPayments: number[] = [0, 0]; // [electricity, water]
+const userDataTuple: [boolean, number, ...string[]] = [true, 40, "John", "alex", "Ann"];
+// userDataTuple[3];
+// userDataTuple.push(50);
+// userDataTuple[3];
 
-const calculatePayments = (
-    {readings,mode}: {readings:number, mode: string},
-    wData: {readings: number},
-    elRate: number,
-    wRate: number
-    ): void => {
-	if (mode === "double" && readings < 50) {
-		monthPayments[0] = readings * elRate * 0.7;
-	} else {
-		monthPayments[0] = readings * elRate;
-	}
+const res = userDataTuple.map((t)=> `${t} - data`);
 
-	monthPayments[1] = wData.readings * wRate;
-};
+const [bthd, age, userName] = userDataTuple;
 
-calculatePayments(electricityUserData, waterUserData, elRate, wRate);
 
-const sendInvoice = (
-    [el,water]:number[],
-     electricityUserData: {readings:number, units: string},
-      waterUserData: {readings: number, units: string}
-      ): string => {
-	const text = `    Hello!
-    This month you used ${electricityUserData.readings} ${electricityUserData.units} of electricity
-    It will cost: ${monthPayments[0]}€
+
+
+
+
+const createError=(msg: string)=>{
+    throw new Error(msg);
+}
+
+
+function logBrtMsg({isBirthdayData,
+                    userNameData,
+                     ageData,
+                    messages:{error}}: {
+    isBirthdayData: boolean,
+    userNameData: string,
+    ageData:number,
+    messages: {error: string}
+}): string {
+    if (isBirthdayData===true) {
+	 return `Congrats ${userNameData.toUpperCase()}, age: ${ageData+1}`;
+    } else {
+        return createError(error);
+    }
     
-    This month you used ${waterUserData.readings} ${waterUserData.units} of water
-    It will cost: ${monthPayments[1]}€`;
 
-	return text;
-};
- console.log(sendInvoice(monthPayments, electricityUserData,waterUserData));
- 
+}
+logBrtMsg(userData);
+
+
+const departments:string[] = ['dev', 'design', 'marketing'];
+
+const department = departments[0];
+// departments.push(5);
+
+const report = departments.filter((d:string)=> d !=='dev')
+                          .map((d:string)=>`${d} -done`);
+
+
+const nums:number[][]= [
+    [55, 0.1, 455], 
+    [2, 45, 57]
+];
+
+const [first]=report;
+console.log(first);
+
 
