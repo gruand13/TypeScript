429f0297725a1e0e536a3b4329cb0dc8a0ed64aa
Andrey Hrusheuski <gruand13@gmail.com>
2023-04-02 12:33:10 +0300

    union type

--- index.ts
+++ index.ts
@@ -1,68 +1,11 @@
-
-const userData={
-    isBirthdayData: true,
-    ageData: 40,
-    userNameData: 'John',
-    messages: {
-        error:"Error"
-    }
-
-}
-
-const userDataTuple: [boolean, number, ...string[]] = [true, 40, "John", "alex", "Ann"];
-// userDataTuple[3];
-// userDataTuple.push(50);
-// userDataTuple[3];
-
-const res = userDataTuple.map((t)=> `${t} - data`);
-
-const [bthd, age, userName] = userDataTuple;
+const message: string | number =5;
+const messages: string[]| number[] = ['a', 'b'];
 
 
+function printMsg (msg: string| number): void {
+    console.log(msg);
+    // если задать метод toLowercase() будет ошибка так как он есть только у строки...
+};
 
-
-
-
-const createError=(msg: string)=>{
-    throw new Error(msg);
-}
-
-
-function logBrtMsg({isBirthdayData,
-                    userNameData,
-                     ageData,
-                    messages:{error}}: {
-    isBirthdayData: boolean,
-    userNameData: string,
-    ageData:number,
-    messages: {error: string}
-}): string {
-    if (isBirthdayData===true) {
-	 return `Congrats ${userNameData.toUpperCase()}, age: ${ageData+1}`;
-    } else {
-        return createError(error);
-    }
-    
-
-}
-logBrtMsg(userData);
-
-
-const departments:string[] = ['dev', 'design', 'marketing'];
-
-const department = departments[0];
-// departments.push(5);
-
-const report = departments.filter((d:string)=> d !=='dev')
-                          .map((d:string)=>`${d} -done`);
-
-
-const nums:number[][]= [
-    [55, 0.1, 455], 
-    [2, 45, 57]
-];
-
-const [first]=report;
-console.log(first);
-
-
+printMsg(4);
+printMsg("adsgfhg");
