eca09f2ea4e0a4ee79ddd87c82f74b2fe271c500
Andrey Hrusheuski <gruand13@gmail.com>
2023-04-02 12:55:43 +0300

    narrowing of types

--- index.ts
+++ index.ts
@@ -2,10 +2,54 @@ const message: string | number =5;
 const messages: string[]| number[] = ['a', 'b'];
 
 
-function printMsg (msg: string| number): void {
-    console.log(msg);
-    // если задать метод toLowercase() будет ошибка так как он есть только у строки...
+// function printMsg (msg: string| number | boolean): void {
+//     if (typeof msg === 'string' || typeof msg === 'number'){
+//         console.log(msg.toString());
+//     } else {
+//         console.log(msg);
+//     }
+//     console.log(msg);
+// };
+
+function printMsg (msg: string[] | number | boolean): void {
+    if (Array.isArray(msg)){
+        msg.forEach(m=> console.log(m));
+    } else if (typeof msg === 'number'){
+        console.log(msg.toFixed());
+    } else {
+        console.log(msg);
+    }
+    
 };
 
+
 printMsg(4);
-printMsg("adsgfhg");
+
+
+const printReadings=(a: number|string, b: number|boolean)=>{
+    if (a===b){
+        console.log(a, b);
+    }
+}
+
+const printReadings2=(a: number[] |string)=>{
+    
+        console.log(a.slice(0,3));
+    
+}
+
+function checkReadings(readings: {system: number} | {user: number}): void {
+    if ('system' in readings){
+        console.log(readings.system);
+    } else {
+        console.log(readings.user);
+    }
+}
+
+function logValue(x: string| Date){
+    if (x instanceof Date){
+        console.log(x.getFullYear());
+    } else {
+        console.log(x.trim());
+    }
+}
