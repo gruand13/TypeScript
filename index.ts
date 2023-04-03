0c5c989b7f7c9401afe81e5b8b128b00ba0552b3
Andrey Hrusheuski <gruand13@gmail.com>
2023-04-03 21:25:52 +0300

    object literals

--- index.ts
+++ index.ts
@@ -1,28 +1,27 @@
 let msg: "Hello" = "Hello";
 msg = 'Hello';
 
-const port3000: number =3000;
-const port3001: number =3001;
-
+const serverConfig: {protocol:"http" | "https"; port:3000 | 3001} = {
+    protocol: 'https',
+    port: 3001
+}
 
 
 //литеральнй тип (четкое значение)
 
-function startServer(
+const startServer: (protocol: "http" | "https",port: 3000 | 3001)=>string = (
     protocol: "http" | "https",
-     port: 3000 | 3001
-     ): 'Server started'{
-        if (port===port3000 || port === port3001){
+    port: 3000 | 3001
+    ): 'Server started'=>{
+       
             console.log(`Server started on ${protocol}://server: ${port}`);
 
-        } else {
-            console.error("Invalid port");
-        }
+    
 
     return "Server started";
 }
 
-startServer('https', 3001);
+startServer(serverConfig.protocol, serverConfig.port);
 
 type AnimationTamingFunc = 'ease' | 'ease-out' | 'ease-in';
 type AnimationId = string | number;
