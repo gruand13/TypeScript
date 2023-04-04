51ebd1acce9c8ab343189d0e95f5b2e714a5ac06
Andrey Hrusheuski <gruand13@gmail.com>
2023-04-04 19:46:44 +0300

    type and intersection

--- index.ts
+++ index.ts
@@ -1,15 +1,31 @@
 let msg: "Hello" = "Hello";
 msg = 'Hello';
 
-const serverConfig: {protocol:"http" | "https"; port:3000 | 3001} = {
-    protocol: 'https',
-    port: 3001
+
+type Config = {protocol:"http" | "https"; port:3000 | 3001};
+type Role ={
+    role: string
 }
 
+type ConfigWithRole = Config & Role;
 
-//литеральнй тип (четкое значение)
+const serverConfig: ConfigWithRole = {
+    protocol: 'https',
+    port: 3001,
+    role: 'admin'
+}
 
-const startServer: (protocol: "http" | "https",port: 3000 | 3001)=>string = (
+const backupConfig : ConfigWithRole = {
+    protocol: 'http',
+    port: 3000,
+    role: 'sysadmin'
+}
+
+type StartFunction = (protocol: "http" | "https", port: 3000 | 3001)=>string;
+
+
+
+const startServer: StartFunction = (
     protocol: "http" | "https",
     port: 3000 | 3001
     ): 'Server started'=>{
