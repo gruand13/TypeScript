b489fe1bb5b2b747d3348bc7b054cac31c903a10
Andrey Hrusheuski <gruand13@gmail.com>
2023-04-04 20:31:27 +0300

    types and interfaces

--- index.ts
+++ index.ts
@@ -1,58 +1,83 @@
-let msg: "Hello" = "Hello";
-msg = 'Hello';
+// type Config = {protocol:"http" | "https"; port:3000 | 3001};
 
+interface Config {
+    protocol:"http" | "https";
+    port:3000 | 3001;
+    log: (msg: string) => void
+}
+// type Role ={
+//     role: string
+// }
 
-type Config = {protocol:"http" | "https"; port:3000 | 3001};
-type Role ={
-    role: string
+interface Role {
+   role: string;
+
 }
 
-type ConfigWithRole = Config & Role;
+interface ConfigWithRole extends Config, Role {}
+// type ConfigWithRole = Config & Role;
 
 const serverConfig: ConfigWithRole = {
     protocol: 'https',
     port: 3001,
-    role: 'admin'
+   role: 'admin',
+   log:(msg: string):void => console.log(msg)
 }
 
-const backupConfig : ConfigWithRole = {
-    protocol: 'http',
-    port: 3000,
-    role: 'sysadmin'
-}
+// const backupConfig : ConfigWithRole = {
+//     protocol: 'http',
+//     port: 3000,
+//     role: 'sysadmin'
+// }
 
-type StartFunction = (protocol: "http" | "https", port: 3000 | 3001)=>string;
+type StartFunction = (
+    protocol: "http" | "https",
+     port: 3000 | 3001,
+    log: (msg: string) => void
+) => string;
 
 
 
 const startServer: StartFunction = (
     protocol: "http" | "https",
-    port: 3000 | 3001
+    port: 3000 | 3001,
+    log: (msg: string) => void
     ): 'Server started'=>{
        
-            console.log(`Server started on ${protocol}://server: ${port}`);
+            log(`Server started on ${protocol}://server: ${port}`);
 
     
 
     return "Server started";
 }
 
-startServer(serverConfig.protocol, serverConfig.port);
+// startServer(serverConfig.protocol, serverConfig.port, serverConfig.log);
 
-type AnimationTamingFunc = 'ease' | 'ease-out' | 'ease-in';
-type AnimationId = string | number;
+// type AnimationTamingFunc = 'ease' | 'ease-out' | 'ease-in';
+// type AnimationId = string | number;
 
-function createAnimation (
-    id: AnimationId, 
-    animationName: string, 
-    timingFunc: AnimationTamingFunc = 'ease',
-    duration: number,
-    iterCount: 'infinite' | number
-    ): void{
-    // const elem = document.querySelector(`#${id}`) as HTMLElement;
-    // if (elem){
-        console.log(`${animationName} ${timingFunc} ${duration} ${iterCount}`);
-        // elem.style.animation = `${animationName} ${timingFunc} ${duration} ${iterCount}`;
-    // }
+// function createAnimation (
+//     id: AnimationId, 
+//     animationName: string, 
+//     timingFunc: AnimationTamingFunc = 'ease',
+//     duration: number,
+//     iterCount: 'infinite' | number
+//     ): void{
+//     // const elem = document.querySelector(`#${id}`) as HTMLElement;
+//     // if (elem){
+//         console.log(`${animationName} ${timingFunc} ${duration} ${iterCount}`);
+//         // elem.style.animation = `${animationName} ${timingFunc} ${duration} ${iterCount}`;
+//     // }
+// }
+//  createAnimation('id', 'fade', 'ease-in', 5, 'infinite');
+
+interface Styles {
+    [key: string]: string
 }
- createAnimation('id', 'fade', 'ease-in', 5, 'infinite');
+
+
+const styles: Styles = {
+    position: "absolute",
+    top: "20px",
+    left: "50px"
+}
