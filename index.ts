ca362dfc52770f6505ec3c8c5e64a23d5c797fc6
Andrey Hrusheuski <gruand13@gmail.com>
2023-04-09 21:52:13 +0300

    read only

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 interface User {
-    login: string,
+    readonly login: string,
     password: string,
     age: number,
     // addr?: string
@@ -10,6 +10,8 @@ interface User {
     }
 }
 
+
+
 const user: User = {
     login: "first!",
     password: "qwerty",
@@ -17,8 +19,36 @@ const user: User = {
     addr: "wqert"
 }
 
+const userFreeze: Readonly<User> = {
+    login: "first!",
+    password: "qwerty",
+    age: 50,
+    addr: "wqert"
+}
+// userFreeze.age= 67;
+
+
+// user.login='fhghj';
+
+
 const dbName = '1235';
 
 function sendUserData(obj: User, db?: string): void{
     console.log(obj.parents?.father?.toLocaleLowerCase(), db?.toLocaleLowerCase())
 }
+
+// class Animal {
+//     readonly name: string ="name";
+// }
+
+
+// дженерик -выделяет все свойства в обьекте и делает их read only
+const basicPorts: ReadonlyArray<number> =[3000, 3001, 3002];
+// basicPorts[0]=5;
+
+// basicPorts.push(566);
+
+// const basicPorts: readonly [number, ...string[]]=[3000, '3001', '3002'];
+// basicPorts[0]=5;
+
+// basicPorts.push(566);
