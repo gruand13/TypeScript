84495cd82db95343cc11200d779a85f495885a15
Andrey Hrusheuski <gruand13@gmail.com>
2023-04-13 21:46:49 +0300

    type request

--- index.ts
+++ index.ts
@@ -1,41 +1,17 @@
-// let smth: unknown;
-
-
-// smth= "str";
-
-// let data: string[]= smth;
-// data.find(e=>e);
-
-// const someValue: unknown=10;
-// someValue.method();
-
-function fetchData(data:unknown): void{
-    if (typeof(data)==='string'){
-        console.log(data.toLowerCase());
-    }
-    
+const dataFromControl = {
+    water: 200,
+    el: 350
 }
 
-const userData=
-"{'isBirthdayData': true, 'ageData': 40, 'userNameData': 'John'}";
-
-
-
-function transferData(d:unknown):  void{
-    if (typeof d==='string'){
-        console.log(d.toLowerCase());
-    } else if (typeof d==='object' && d ){
-        console.log(d);
-    }else{
-        console.error('Some error');
-    }
+function checkReadings (data: typeof dataFromControl): boolean {
+    const dataFromUser={
+        water: 200,
+        el: 350
+    };
+    if (data.el === dataFromUser.el && data.water === dataFromUser.water){
+        return true;
+    } else return false;
 }
 
-transferData(userData);
-
-
-type T0 = any| unknown;
-type T1 = number| unknown;
-
-type T2 = any& unknown;
-type T3 = number & unknown;
+const PI = 3.14;
+let PIClone: typeof PI;
