39666865552b11fca43c59facbd148aec5b564fc
Andrei Grusheuski <gruand13@gmail.com>
2023-04-30 14:08:39 +0300

    overload of function

--- index.ts
+++ index.ts
@@ -1,87 +1,33 @@
-function printMsg (msg: string[] | number | boolean): void{
-    if (Array.isArray(msg)){
-        msg.forEach((m)=> console.log(m));
-    } else if (isNumber(msg)){
-        console.log(msg);
+interface Square {
+    side: number;
+    area: number;
+}
+
+interface Rect {
+    a: number;
+    b: number;
+    area: number;
+}
+
+// overload  function
+function calculateArea(side: number): Square;
+function calculateArea(a: number, b: number): Rect;
+
+function calculateArea(a: number, b?: number): Square | Rect {
+    if (b){
+        const rect: Rect ={
+            // a: a, b:b
+            a, b,
+            area: a*b
+        }
+        return rect;
     } else {
-        console.log(msg)
-    }
-    console.log(msg);
-}
-printMsg(4);
-
-//Type guard
-
-// function isNumber(n: string[] | number| boolean): n is number {
-//     return typeof n === 'number';
-// }
-
-function isNumber(n: unknown): n is number {
-    return typeof n === 'number';
-}
-
-
-interface Car {
-    name: 'car';
-    engine: string,
-    wheels: {
-        number: number;
-        type: string;
+        const square: Square = {
+            side: a,
+            area: a*a
+        }
+        return square;
     }
 }
-
-interface Ship {
-    name: 'ship';
-    engine: string,
-    sail: string
-}
-
-interface Airplane {
-    name: 'airplane';
-    engine: string;
-    wings: string;
-}
-
-interface SuperAirplane{
-    name: 'smth',
-    engine: string,
-    wings: string
-}
-type Vehicle = Car | Ship | Airplane | SuperAirplane;
-
-function isCar(car: Vehicle) : car is Car {
-    // return 'wheels' in car;
-    return (car as Car).wheels.number !== undefined;
-}
-
-function isShip(ship: Vehicle) : ship is Ship{
-    return 'sail' in ship;
-    
-}
-
-function repairVehicle(vehicle: Vehicle) {
-    // if (isCar(vehicle)){
-    //     vehicle.wheels
-    // } else if (isShip(vehicle)){
-    //     vehicle;
-    // } else {
-    //     vehicle.wings;
-    // }
-
-    switch(vehicle.name){
-        case 'car':
-            console.log(vehicle.wheels);
-            break;
-        case 'ship':
-            console.log(vehicle.sail);
-            break;
-        case 'airplane':
-            console.log(vehicle.wings);
-            break;
-        case "smth":
-            console.log(vehicle.wings);
-            break;
-        default:
-            const smth: never = vehicle;
-            console.log("OOOPS!");
-}
+calculateArea(4);
+calculateArea(1, 5);
