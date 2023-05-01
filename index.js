361cd1b82baef3608847388436bf0c1db70e6bf0
Andrei Grusheuski <gruand13@gmail.com>
2023-05-01 10:43:52 +0300

    homework with dom

--- index.js
+++ index.js
@@ -1,28 +1,48 @@
-function isCar(car) {
-    // return 'wheels' in car;
-    return car.wheels.number !== undefined;
-}
-function isShip(ship) {
-    return 'sail' in ship;
-}
-var car = {
-    name: 'car',
-    engine: 'V8'
+var forms = document.querySelectorAll('form');
+var email = document.querySelector('#email');
+var title = document.querySelector('#title');
+var text = document.querySelector('#text');
+var checkbox = document.querySelector('#checkbox');
+var formData = {
+    email: "",
+    title: "",
+    text: "",
+    checkbox: false,
 };
-function repairVehicle(vehicle) {
-    switch (vehicle.name) {
-        case 'car':
-            console.log(vehicle.wheels);
-            break;
-        case 'ship':
-            console.log(vehicle.sail);
-            break;
-        case 'airplane':
-            console.log(vehicle.wings);
-            break;
-        default:
-            // const smth: never = vehicle;
-            console.log("OOOPS!");
+forms.forEach(function (form) {
+    return form.addEventListener("submit", function (e) {
+        var _a, _b, _c, _d;
+        e.preventDefault();
+        formData.email = (_a = email === null || email === void 0 ? void 0 : email.value) !== null && _a !== void 0 ? _a : "";
+        formData.title = (_b = title === null || title === void 0 ? void 0 : title.value) !== null && _b !== void 0 ? _b : "";
+        formData.text = (_c = text === null || text === void 0 ? void 0 : text.value) !== null && _c !== void 0 ? _c : "";
+        formData.checkbox = (_d = checkbox === null || checkbox === void 0 ? void 0 : checkbox.checked) !== null && _d !== void 0 ? _d : false;
+        if (validateFormData(formData)) {
+            checkFormData(formData);
+        }
+    });
+});
+function validateFormData(data) {
+    if (Object.values(data).every(function (value) { return value; })) {
+        return true;
+    }
+    else {
+        return false;
     }
 }
-repairVehicle(car);
+// Последовательность действий:
+// 1) Происходит submit любой из форм
+// 2) Все данные из 4х полей со страницы переходят в свойства объекта formData
+// 3) Запускается функция validateFormData с этим объектом, возвращает true/false
+// 4) Если на предыдущем этапе true, то запускается функция checkFormData с этим объектом
+function checkFormData(data) {
+    var email = data.email;
+    var emails = ["example@gmail.com", "example@ex.com", "admin@gmail.com"];
+    // Если email совпадает хотя бы с одним из массива
+    if (emails.some(function (e) { return e === email; })) {
+        console.log("This email is already exist");
+    }
+    else {
+        console.log("Posting data...");
+    }
+}
--- index.ts
+++ index.ts
@@ -1,18 +1,64 @@
-const box= document.querySelector('.box') as HTMLElement;
+const forms = document.querySelectorAll('form');
+const email = document.querySelector('#email') as HTMLInputElement;
+const title = document.querySelector('#title') as HTMLInputElement;
+const text = document.querySelector('#text') as HTMLTextAreaElement;
+const checkbox = document.querySelector('#checkbox') as HTMLInputElement;
 
-const input = document.querySelector('input');
-const link  = document.querySelector('a');
-const p = document.querySelector('.paragraph') as HTMLParagraphElement;
-
-const links = document.querySelectorAll('a');
-
-if (link){
-	link.href = 'sgdfhg';
+interface IFormData{
+	email: string;
+	title: string;
+	text: string;
+	checkbox: boolean;
 }
-input?.value;
 
-const elem = document.createElement('a');
+const formData: IFormData = {
+	email: "",
+	title: "",
+	text: "",
+	checkbox: false,
+};
 
-link?.addEventListener('click', (e)=> {
-	e.preventDefault();
-})
+forms.forEach((form)=>
+	form.addEventListener("submit", (e)=>{
+		e.preventDefault();
+
+		formData.email = email?.value ?? "";
+		formData.title = title?.value ?? "";
+		formData.text = text?.value ?? "";
+		formData.checkbox = checkbox?.checked ?? false;
+
+			if (validateFormData(formData)){
+				checkFormData(formData);
+			}
+
+		})
+	)
+
+	function validateFormData(data: IFormData): boolean {
+		if (Object.values(data).every((value) => value)){
+			return true;
+
+		} else {
+			return false;
+		}
+
+	}
+// Последовательность действий:
+// 1) Происходит submit любой из форм
+// 2) Все данные из 4х полей со страницы переходят в свойства объекта formData
+// 3) Запускается функция validateFormData с этим объектом, возвращает true/false
+// 4) Если на предыдущем этапе true, то запускается функция checkFormData с этим объектом
+
+
+
+function checkFormData(data: IFormData) {
+	const { email } = data;
+	const emails = ["example@gmail.com", "example@ex.com", "admin@gmail.com"];
+
+	// Если email совпадает хотя бы с одним из массива
+	if (emails.some((e)=> e===email)) {
+		console.log("This email is already exist");
+	} else {
+		console.log("Posting data...");
+	}
+}
