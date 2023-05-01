0f123d44dbdb98d14ca750cffad3a76040b1dce2
Andrei Grusheuski <gruand13@gmail.com>
2023-05-01 08:35:20 +0300

    practise task

--- index.ts
+++ index.ts
@@ -1,71 +1,54 @@
-interface Car {
-    name: 'car';
-    engine: string,
-    wheels: {
-        number: number;
-        type: string;
-    }
-}
-interface Ship {
-    name: 'ship';
-    engine: string,
-    sail: string
+// Request
+type Animal ='cat' | 'dog' | 'bird';
+
+enum AnimalStatus {
+	Available = 'available',
+	NotAvailable = 'not available',
 }
 
-interface Airplane {
-    name: 'airplane';
-    engine: string;
-    wings: string;
+interface AnimalData	{
+		animal: Animal;
+		breed: string;
+		sterilized?: string;
+	}
+
+
+interface AnimalAvailableData extends AnimalData	{
+		location: string;
+		age?: number;
+		
+	}
+
+
+interface AnimalNotAvailableData	{
+	message: string;
+	nextUpdateIn: Date;
 }
 
-interface SuperAirplane{
-    name: 'smth',
-    engine: string,
-    wings: string
+interface AnimalAvailableResponse {
+	status: AnimalStatus.Available;
+	data: AnimalAvailableData;
+}
+interface AnimalNotAvailableResponse {
+	status: AnimalStatus.NotAvailable;
+	data: AnimalNotAvailableData;
 }
 
-interface ComplexVehicle{
-    name : 'car'| 'ship'| 'airplane';
-    engine: string;
-    wheels?:number;
-    sail?: string;
-    wings?: string;
-}
-type Vehicle = Car | Ship | Airplane ;
+type Res =AnimalAvailableResponse | AnimalNotAvailableResponse;
 
-function isCar(car: Vehicle) : car is Car {
-    // return 'wheels' in car;
-    return (car as Car).wheels.number !== undefined;
+function isAvailable(res: Res): res is AnimalAvailableResponse {
+	if (res.status === AnimalStatus.Available){
+		return true;
+	} else {
+		return false;
+	}
 }
 
-function isShip(ship: Vehicle) : ship is Ship{
-    return 'sail' in ship;
-
+function checkAnimalData(animal: Res): AnimalAvailableData | string {
+	if (isAvailable(animal)) {
+		
+		return animal.data;
+	} else {
+		return `${animal.data}, you can try in ${animal.data.nextUpdateIn}`;
+	}
 }
-
-const car: ComplexVehicle = {
-    name: 'car',
-    engine: 'V8',
-};
-
-function repairVehicle(vehicle: ComplexVehicle) {
-
-    switch(vehicle.name){
-        case 'car':
-            console.log(vehicle.wheels! * 2);
-            break;
-        case 'ship':
-            console.log(vehicle.sail);
-            break;
-        case 'airplane':
-            console.log(vehicle.wings);
-            break;
-        
-        default:
-            // const smth: never = vehicle;
-
-            console.log("OOOPS!");
-    }
-}
-
-repairVehicle(car);
