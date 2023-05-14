34ef0e08f3fd495ec5639d41b4370f5f83529ba4
Andrei Grusheuski <gruand13@gmail.com>
2023-05-14 11:40:39 +0300

    generics

--- index.ts
+++ index.ts
@@ -1,64 +1,36 @@
-const forms = document.querySelectorAll('form');
-const email = document.querySelector('#email') as HTMLInputElement;
-const title = document.querySelector('#title') as HTMLInputElement;
-const text = document.querySelector('#text') as HTMLTextAreaElement;
-const checkbox = document.querySelector('#checkbox') as HTMLInputElement;
+function processingData<T>(data:T):T { 
 
-interface IFormData{
-	email: string;
-	title: string;
-	text: string;
-	checkbox: boolean;
+	return data;
 }
 
-const formData: IFormData = {
-	email: "",
-	title: "",
-	text: "",
-	checkbox: false,
-};
+let res1 = processingData(1);
+const res2 = processingData('1');
+const num=10;
+const res3 = processingData<number>(num);
 
-forms.forEach((form)=>
-	form.addEventListener("submit", (e)=>{
-		e.preventDefault();
-
-		formData.email = email?.value ?? "";
-		formData.title = title?.value ?? "";
-		formData.text = text?.value ?? "";
-		formData.checkbox = checkbox?.checked ?? false;
-
-			if (validateFormData(formData)){
-				checkFormData(formData);
-			}
-
-		})
-	)
-
-	function validateFormData(data: IFormData): boolean {
-		if (Object.values(data).every((value) => value)){
-			return true;
-
-		} else {
-			return false;
-		}
-
-	}
-// Последовательность действий:
-// 1) Происходит submit любой из форм
-// 2) Все данные из 4х полей со страницы переходят в свойства объекта formData
-// 3) Запускается функция validateFormData с этим объектом, возвращает true/false
-// 4) Если на предыдущем этапе true, то запускается функция checkFormData с этим объектом
-
-
-
-function checkFormData(data: IFormData) {
-	const { email } = data;
-	const emails = ["example@gmail.com", "example@ex.com", "admin@gmail.com"];
-
-	// Если email совпадает хотя бы с одним из массива
-	if (emails.some((e)=> e===email)) {
-		console.log("This email is already exist");
-	} else {
-		console.log("Posting data...");
-	}
+interface PrintUK {
+	design: number;
 }
+
+interface PrintES {
+	design: string;
+}
+
+interface Print<T> {
+	design: T;
+}
+
+const somePrint: Print<string> = {
+	design: 'ten'
+}
+const someOtherPrint: Print<number> = {
+	design: 10
+}
+
+
+// Array<T>
+
+// RefferalSystem<UserID,UserRefferals>
+
+
+// T U V S P K/V 
