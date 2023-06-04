c302c0fd2635686aab4f9d76f09d6772f5c4dbec
Andrei Grusheuski <gruand13@gmail.com>
2023-06-04 12:28:09 +0300

    utility types: pick, omit, extract, exclude, Record

--- index.ts
+++ index.ts
@@ -5,8 +5,34 @@ type Currencies = {
 	kz: 'tenge'
 }
 
+type CurrWithoutUSA = Omit<Currencies, 'usa'> // delete selected property
+type CurrUSAAndUkraine = Pick<Currencies, 'usa' | 'ukraine'> // filtration by properties
+type FadeType = Exclude<MyAnimation, 'swipe'> // delete from union type
+type CountiresWithoutUSA = Exclude<keyof Currencies, 'usa'>
+
+type Swipetype = Extract<MyAnimation | Direction, 'swipe'> // select pointed type
+
+
 type CreateCustomCurr<T> = {
 	[P in keyof T as `custom${Capitalize< string & P>}`] :  string
+}
+
+type PlayersNames = 'Alex' | 'John';
+type GameDataCurr = Record<PlayersNames, CustomCurrencies>  //
+
+const gameData : GameDataCurr = {
+	Alex: {
+		customChina: 'qqqq',
+		customKz: 'ww',
+		customUkraine: 'ss',
+		customUsa: 'qqq'
+	},
+	John: {
+		customChina: 'qqqq',
+		customKz: 'ww',
+		customUkraine: 'ss',
+		customUsa: 'qqq'
+	}
 }
 
 type CustomCurrencies = CreateCustomCurr<Currencies>
