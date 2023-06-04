994ded86d2182308fad4824c582ff54aee18cc3e
Andrei Grusheuski <gruand13@gmail.com>
2023-06-04 15:58:39 +0300

    additional utility types(return type,Parameters

--- index.ts
+++ index.ts
@@ -1,109 +1,16 @@
-// Необходимо типизировать этот большой объект
-// Свойство futureClasses должно быть в зависимости от classes по типу
-// Свойства exClients и futureClients тоже должны быть в зависимости от currClients
-// ИЛИ все три зависят от общего родителя
-
-// Простыми словами: при добавлении свойства в целевой объект они должны быть
-// автоматически добавлены в зависимые (сразу подсказка от TS)
-
-interface IFitnessClass {
-	name: string,
-	startsAt: string,
-	duration: number,
+function calculate(a:number, b: number): number {
+	return a*b;
 }
 
-interface IFutureClass extends Omit<IFitnessClass, 'startsAt'> {
-	willStartsAt: string
+type CalculateRT = ReturnType<typeof calculate>;
+
+let anotherRes: CalculateRT = 5;
+
+type CalculatePT = Parameters<typeof calculate>[0];
+type PT1 = Parameters<(a: number)=> number>;
+type PT2 = Parameters<<T>(arg: T)=>T >;
+
+class Example {
+	constructor(a: number){}
 }
-
-interface IClient {
-	name: string,
-	age: string | number,
-	gender: 'male'| 'female',
-	timeLeft: string,
-	makeCallFor: Date,
-	
-}
-
-type CurrClent = Omit<IClient, 'makeCallFor'>;
-type ExClients = Omit<IClient, 'timeLeft'>;
-type FutureClients = Pick<IClient, 'name' | 'makeCallFor'>;
-
-interface IFitnessClub {
-	clubName: string,
-	location: string,
-	classes: IFitnessClass[],
-	futureClasses: IFutureClass[],
-	currClients: CurrClent[],
-	exClients: ExClients[],
-	futureClients: FutureClients[]
-}
-
-
-const fitnessClubCenter: IFitnessClub = {
-	clubName: "Fitness club Center",
-	location: "central ave. 45, 5th floor",
-	classes: [
-		{
-			name: "yoga",
-			startsAt: "8:00 AM",
-			duration: 60,
-		},
-		{
-			name: "trx",
-			startsAt: "11:00 AM",
-			duration: 45,
-		},
-		{
-			name: "swimming",
-			startsAt: "3:00 PM",
-			duration: 70,
-		},
-	],
-	futureClasses: [
-		{
-			name: "boxing",
-			willStartsAt: "6:00 PM",
-			duration: 40,
-		},
-		{
-			name: "breath training",
-			willStartsAt: "8:00 PM",
-			duration: 30,
-		},
-	],
-	currClients: [
-		{
-			name: "John Smith",
-			age: "-",
-			gender: "male",
-			timeLeft: "1 month",
-		},
-		{
-			name: "Alise Smith",
-			age: 35,
-			gender: "female",
-			timeLeft: "3 month",
-		},
-		{
-			name: "Ann Sonne",
-			age: 24,
-			gender: "female",
-			timeLeft: "5 month",
-		},
-	],
-	exClients: [
-		{
-			name: "Tom Smooth",
-			age: 50,
-			gender: "male",
-			makeCallFor: new Date("2023-08-12"),
-		},
-	],
-	futureClients: [
-		{
-			name: "Maria",
-			makeCallFor: new Date("2023-07-10"),
-		},
-	],
-};
+type T0 = ConstructorParameters<typeof Example>
